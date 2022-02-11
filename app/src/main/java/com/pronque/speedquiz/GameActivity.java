07277e25de7d590a4b19e184d80638818dfe3380
Quentin Prongué <quentin.prongue@divtec.ch>
2022-02-11 08:45:37 +0100

    Interface + début Java

--- app/src/main/java/com/pronque/speedquiz/GameActivity.java
+++ app/src/main/java/com/pronque/speedquiz/GameActivity.java
@@ -6,9 +6,15 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class GameActivity extends AppCompatActivity {
     private TextView TV_name_player1;
     private TextView TV_name_player2;
+    private TextView TV_question_player1;
+    private TextView TV_question_player2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +24,8 @@ public class GameActivity extends AppCompatActivity {
 
         TV_name_player1 = findViewById(R.id.name_player1);
         TV_name_player2 = findViewById(R.id.name_player2);
+        TV_question_player1 = findViewById(R.id.question_player_1);
+        TV_question_player2 = findViewById(R.id.question_player_2);
     }
 
     @Override
@@ -30,9 +38,17 @@ public class GameActivity extends AppCompatActivity {
         // Récupère le nom des joueurs
         String namePlayer1 = extras.getString("namePlayer1");
         String namePlayer2 = extras.getString("namePlayer2");
+        ArrayList<String> listQuestions = extras.getStringArrayList("listQuestions");
+
+        listQuestions.add("Internet a été crée en 1969");
+        listQuestions.add("Il y a 7 bits dans 1 octet");
 
         // Défini le texte pour les TextViews
         TV_name_player1.setText(namePlayer1);
         TV_name_player2.setText(namePlayer2);
+
+        TV_question_player1.setText(listQuestions.get(ThreadLocalRandom.current().nextInt(0, listQuestions.size() - 1)));
+
+        TV_question_player2.setText(listQuestions.get(ThreadLocalRandom.current().nextInt(0, listQuestions.size() - 1)));
     }
 }
--- app/src/main/java/com/pronque/speedquiz/MainActivity.java
+++ app/src/main/java/com/pronque/speedquiz/MainActivity.java
@@ -1,8 +1,10 @@
 package com.pronque.speedquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
@@ -15,15 +17,26 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     private Button BT_add_player;
     private Button BT_start_new_game;
+    private Button BT_settings_apply;
+    private Button BT_settings_cancel;
+    private Button BT_questions_apply;
+    private Button BT_questions_cancel;
+    // private  SWITCH_night_mode;
     private LinearLayout LL_player;
     private FrameLayout FL_settings;
     private FrameLayout FL_questions;
     private EditText ET_name_player1;
     private EditText ET_name_player2;
+    private EditText ET_add_question;
+
+    private ArrayList<String> listeQuestions = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +47,17 @@ public class MainActivity extends AppCompatActivity {
 
         BT_add_player = findViewById(R.id.bt_add_player);
         BT_start_new_game = findViewById(R.id.bt_start_new_game);
+        BT_settings_apply = findViewById(R.id.bt_settings_apply);
+        BT_settings_cancel = findViewById(R.id.bt_settings_cancel);
+        BT_questions_apply = findViewById(R.id.bt_questions_apply);
+        BT_questions_cancel = findViewById(R.id.bt_questions_cancel);
+        // SWITCH_night_mode = findViewById(R.id.switch_night_mode);
         LL_player = findViewById(R.id.linearlayout_player);
         FL_settings = findViewById(R.id.framelayout_settings);
         FL_questions = findViewById(R.id.framelayout_questions);
         ET_name_player1 = findViewById(R.id.editext_name_player1);
         ET_name_player2 = findViewById(R.id.editext_name_player2);
+        ET_add_question = findViewById(R.id.editext_new_question);
     }
 
     @Override
@@ -77,6 +96,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /*
+        SWITCH_night_mode.setOnSwitchListener(new NightModeButton.OnSwitchListener() {
+            @Override
+            public void onSwitchListener(boolean isNight) {
+                if(isNight){
+                    animateBackground(colorFrom,colorTo);
+                    animateStatusActionBar(getResources().getColor(R.color.colorPrimary),colorTo);
+                    Toast.makeText(getApplicationContext(),"Night Mode On",Toast.LENGTH_SHORT).show();
+                } else {
+                    animateBackground(colorTo,colorFrom);
+                    animateStatusActionBar(colorTo,getResources().getColor(R.color.colorPrimary));
+                    Toast.makeText(getApplicationContext(),"Night Mode Off",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+         */
+
         BT_add_player.setOnClickListener(v -> {
             BT_add_player.setVisibility(View.INVISIBLE);
             LL_player.setVisibility(View.VISIBLE);
@@ -89,9 +126,42 @@ public class MainActivity extends AppCompatActivity {
 
             // Permet d'appeller une activité
             Intent i = new Intent(getApplicationContext(), GameActivity.class);
+            i.putExtra("listQuestions", listeQuestions);
             i.putExtra("namePlayer1", namePlayer1);
             i.putExtra("namePlayer2", namePlayer2);
             startActivity(i);
+        });
+
+        BT_settings_cancel.setOnClickListener(v -> {
+            LL_player.setVisibility(View.GONE);
+            FL_settings.setVisibility(View.GONE);
+            FL_questions.setVisibility(View.GONE);
+            BT_add_player.setVisibility(View.VISIBLE);
+        });
+
+        BT_questions_cancel.setOnClickListener(v -> {
+            LL_player.setVisibility(View.GONE);
+            FL_settings.setVisibility(View.GONE);
+            FL_questions.setVisibility(View.GONE);
+            BT_add_player.setVisibility(View.VISIBLE);
+        });
+
+        BT_settings_apply.setOnClickListener(v -> {
+            Toast.makeText(this, "Vos paramètres ont bien été sauvegardés", Toast.LENGTH_SHORT).show();
+            BT_add_player.setVisibility(View.VISIBLE);
+            LL_player.setVisibility(View.GONE);
+            FL_settings.setVisibility(View.GONE);
+            FL_questions.setVisibility(View.GONE);
+        });
+
+        BT_questions_apply.setOnClickListener(v -> {
+            Toast.makeText(this, ET_add_question.getText().toString(), Toast.LENGTH_SHORT).show();
+            listeQuestions.add(ET_add_question.getText().toString());
+            Toast.makeText(this, "Votre question a bien été ajouté", Toast.LENGTH_SHORT).show();
+            BT_add_player.setVisibility(View.VISIBLE);
+            LL_player.setVisibility(View.GONE);
+            FL_settings.setVisibility(View.GONE);
+            FL_questions.setVisibility(View.GONE);
         });
     }
 
@@ -102,6 +172,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
